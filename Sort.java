df490ee96d37028802cf09a16d8bbff30fca6a9a
purlin07 <ghan@g.clemson.edu>
2014-12-21 22:57:49 -0500

    add shell's sort

--- Sort.java
+++ Sort.java
@@ -1,22 +1,25 @@
 public class Sort{
 	public static void main(String[] args){
 		System.out.println("Sort");
-		int arr[] = {3,5,1,2,7,6};
+		int arr[] = {3,9,1,2,7,6};
         print(arr);
-		insertionSort(arr);
-		print(arr);
+	//	System.out.println("Insertion Sort");
+	//	insertionSort(arr);
+		System.out.println("Shell Insertion Sort");
+		shellSort(arr);
+//		shellInsertionSort(arr,3);
+		//print(arr);
 	}
 
 	public static void insertionSort(int arr[]){
 		int i,j,n;
 		for(i=1;i<arr.length;i++){
 			n=arr[i];
-			j=i;
 //			while(j>0&&arr[j-1]>n){
 //				arr[j]=arr[j-1];
 //				j--;
 //			}
-            for(;j>0;j--){
+            for(j=i;j>=1;j--){
 				if (arr[j-1]>n){
 					arr[j]=arr[j-1];
 				}else{
@@ -24,6 +27,31 @@ public class Sort{
 				}
 			}			
 			arr[j]=n;
+			print(arr);
+		}
+	}
+
+
+	public static void shellSort(int arr[]){
+		int gap = arr.length/2;
+		while(gap>=1){
+			shellInsertionSort(arr,gap);
+			gap=gap/2;
+		}
+	}
+	public static void shellInsertionSort(int arr[],int gap){
+		int i,j,n;
+		for(i=gap;i<arr.length;i++){
+			n=arr[i];
+			for(j=i;j>=gap;j=j-gap){
+				if(arr[j-gap]>n){
+					arr[j]=arr[j-gap];
+				}else{
+					break;
+				}
+			}
+			arr[j]=n;
+			print(arr);
 		}
 	}
 
