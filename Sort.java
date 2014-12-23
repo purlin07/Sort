7fd64f012791c004f72c6dab47acf28f681c7b75
purlin07 <ghan@g.clemson.edu>
2014-12-23 15:44:11 -0500

    add quick sort&simple selection sort&bubble sort

--- Sort.java
+++ Sort.java
@@ -1,14 +1,27 @@
+
 public class Sort{
+
 	public static void main(String[] args){
 		System.out.println("Sort");
-		int arr[] = {3,9,1,2,7,6};
-        print(arr);
-	//	System.out.println("Insertion Sort");
-	//	insertionSort(arr);
+		int arr[] = getArr(10);
+		System.out.println("Insertion Sort");
+		insertionSort(arr);
+
+		arr = getArr(10);
 		System.out.println("Shell Insertion Sort");
 		shellSort(arr);
-//		shellInsertionSort(arr,3);
-		//print(arr);
+
+		arr = getArr(10);
+		System.out.println("Bubble Sort");
+		bubbleSort(arr);
+
+		arr = getArr(10);
+		System.out.println("Simple Selection Sort");
+		simpleSelectionSort(arr);
+
+		arr = getArr(10);
+		System.out.println("Quick Sort");
+		quickSort(arr,0,arr.length-1);		
 	}
 
 	public static void insertionSort(int arr[]){
@@ -39,6 +52,7 @@ public class Sort{
 			gap=gap/2;
 		}
 	}
+
 	public static void shellInsertionSort(int arr[],int gap){
 		int i,j,n;
 		for(i=gap;i<arr.length;i++){
@@ -54,11 +68,91 @@ public class Sort{
 			print(arr);
 		}
 	}
+	
+	public static void simpleSelectionSort(int arr[]){
+		int min,tem;
+		for(int i=0;i<arr.length;i++){
+			min=i;
+			for(int j=i+1;j<arr.length;j++){
+				if(arr[min]>arr[j]){
+					min=j;
+				}	
+			}
+			if(min!=i){
+				tem=arr[i];
+				arr[i]=arr[min];
+				arr[min]=tem;
+			}
+			print(arr);
+		}
+	}
+
+	public static void bubbleSort(int arr[]){
+		for(int i=0;i<arr.length-1;i++){
+			for(int j=0;j<arr.length-i-1;j++){
+				if(arr[j]>arr[j+1]){
+					int tem=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=tem;
+				}
+			}
+			print(arr);
+		}
+	}
+
+	public static void quickSort(int arr[],int left,int right){		
+		System.out.println("============recursion==============");
+		print(arr);
+		int index = quickSortPatition(arr,left,right);
+		System.out.println("index = " + index);
+		if(left < index - 1){
+			quickSort(arr,left,index-1);
+		}
+		if(right > index){
+			quickSort(arr,index,right);
+		}
+	}
+
+	public static int quickSortPatition(int arr[],int left,int right){
+		int tem;
+		int pivot = arr[left];
+		System.out.println("pivot = "+pivot);
+		while(left<=right){
+			while(arr[left]<pivot){
+				left++;
+			}
+			while(arr[right]>pivot){
+				right--;
+			}
+			if(left<=right){
+				tem = arr[right];
+				arr[right]=arr[left];
+				arr[left]=tem;
+				System.out.println("left="+left+" right="+right+" swap");
+				print(arr);
+				left++;
+				right--;
+			}
+		}
+		return left;
+	}
+
+
 
 	public static void print(int arr[]){
 		for(int i = 0; i< arr.length; i++){
 			System.out.print(arr[i]+" ");
 		}
 		System.out.print("\n");
+	}
+
+	public static int[] getArr(int size){
+		int[] arr = new int[size];
+		int item=0;
+		for(int i =0; i<size; i++){
+			item=(int)(Math.random()*100);
+			arr[i]= item;
+		}
+		return arr;
 	}
 }
